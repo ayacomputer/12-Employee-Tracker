18b353fe493260bb3c88e797b2f1e95f4d03816e
Ayako Woollan <ayako.woollan@gmail.com>
2022-02-16 12:42:04 +1100

    adding basic info to seeds.sql and  getAllEmployees() works

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,24 @@
-INSERT 
-VALUE
+USE employees_db;
+INSERT INTO department (name)
+VALUES ("department A"),
+("department B"),
+("department C"),
+("department D"),
+("department E");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("role A", 100,1),
+       ("role B", 200,2),
+       ("role C", 300,3),
+       ("role D", 400,4),
+       ("role E", 500,5);
+
+       
+
+INSERT INTO employee (first_name, last_name,role_id)
+VALUES ("Elliot", "Smith",1),
+       ("Amira", "Afzal",2),
+       ("Christoper", "Lee",3),
+       ("Verónica", "Rodriguez",4),
+       ("Igor", "Stein",5);
+       
--- index.js
+++ index.js
@@ -1,12 +1,56 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const cTable = require('console.table');
+require('dotenv').config();
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+    },
+    console.log(`Connected to the movies_db database.`)
+);
+
+const questions = [
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'initial',
+        choices: ['View All Employees', 'A', 'B', 'C'],
+    },
+];
 
 
 
-const questions = [{
 
-
-
-}];
 
 const promptUser = () => inquirer.prompt(questions);
+
+promptUser().then((data) => {
+    console.log(data.initial);
+    switch (data.initial) {
+        case 'View All Employees':
+            return getAllEmployees();
+        case 'A':
+            return console.log('A is chosen')
+        default: console.log('default')
+    }
+
+}
+)
+
+
+getAllEmployees = () => {
+    const sql = `SELECT * FROM employee`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            return console.error('Something went wrong', err);
+        }
+        console.table(rows);
+    });
+
+
+};
