bf8d0fb09a989aa37157b69ed5b3d9a0afbecaad
Ayako Woollan <ayako.woollan@gmail.com>
2022-02-19 16:42:51 +1100

    query.sql

--- db/query.sql
+++ db/query.sql
@@ -1,7 +1,11 @@
--- USE employees_db;
--- DROP TABLE IF EXISTS allInfo;
--- CREATE TABLE allInfo AS(
---     SELECT employee.id AS id, CONCAT(employee.first_name," ",employee.last_name) AS 'Full Name', department.name AS 'department', role.title AS 'role', role.salary AS 'salary', manager_id AS manager_id
--- FROM employee
--- JOIN role ON employee.role_id = role.id
--- JOIN department ON role.department_id = department.id);
+USE employees_db;
+CREATE TABLE allInfo AS(
+    SELECT employee.id AS id, 
+    CONCAT(employee.first_name," ",employee.last_name) AS 'Full Name', 
+    department.name AS 'department', 
+    role.title AS 'role', 
+    role.salary AS 'salary', 
+    manager_id AS manager_id
+FROM employee
+JOIN role ON employee.role_id = role.id
+JOIN department ON role.department_id = department.id);
--- db/schema.sql
+++ db/schema.sql
@@ -26,11 +26,5 @@ CREATE TABLE employee (
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-    FOREIGN KEY (manager_id) REFERENCES employee(id) 
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
-
-CREATE TABLE allInfo AS(
-    SELECT employee.id AS id, CONCAT(employee.first_name," ",employee.last_name) AS 'Full Name', department.name AS 'department', role.title AS 'role', role.salary AS 'salary', manager_id AS manager_id
-FROM employee
-JOIN role ON employee.role_id = role.id
-JOIN department ON role.department_id = department.id);
