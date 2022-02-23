829361a574b46ed4fcf11e2c51565552b0ebcc85
Ayako Woollan <ayako.woollan@gmail.com>
2022-02-23 12:31:26 +1100

    working on getEmployeeByManager

--- index.js
+++ index.js
@@ -665,22 +665,24 @@ const getEmployeeByManager = () => {
 
         }]).then((results) => {
             console.log(results);
-            const sql = `SELECT employee.id AS ID,
-                CONCAT (employee.first_name, " ", employee.last_name) AS Name,
-                role.title AS Title, 
-                department.name AS Department,
-                role.salary AS Salary, 
-                CONCAT (manager.first_name, " ", manager.last_name) AS Manager, 
-                FROM employee
-                LEFT JOIN role ON employee.role_id = role.id
-                LEFT JOIN department ON role.department_id = department.id
-                LEFT JOIN employee manager ON employee.manager_id = manager.id
-                WHERE manager_id = ${results.manager};`;
+            const sql = `
+            SELECT employee.id AS ID, 
+            CONCAT (employee.first_name, " ", employee.last_name) AS Name,
+            role.title AS Title, 
+            department.name AS Department,
+            role.salary AS Salary, 
+            CONCAT (manager.first_name, " ", manager.last_name) AS Manager
+            FROM employee
+            LEFT JOIN role ON employee.role_id = role.id
+            LEFT JOIN department ON role.department_id = department.id
+            LEFT JOIN employee manager ON employee.manager_id = manager.id
+            WHERE employee.manager_id = ${results.manager};`;
             db.query(sql, (err, rows) => {
                 if (err) {
                     return console.error('Something went wrong', err);
                 }
                 printTable(rows);
+                init();
             });
 
         })
